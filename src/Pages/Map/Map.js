b70e1457fc01ea4f81df122ad5176e9dfdb22d4c
Kennedy <kennedymnovas@gmail.com>
2019-09-15 03:01:06 -0300

    ajustes mapa

--- src/Pages/Map/Map.js
+++ src/Pages/Map/Map.js
@@ -3,19 +3,9 @@ import {BackHandler, View, Text, Dimensions, Alert} from 'react-native'
 import DiscardButton from './DiscardButton'
 import {  ButtonView, Container } from './styles'
 import React, {useState, useEffect} from 'react';
-import LottieView from 'lottie-react-native';
-import AnimatedLoader from 'react-native-animated-loader'
 
 
 export default function Main( { navigation } ) {
-    const [isLoading, setIsLoading] = useState(true);
-
-    const toggleLoader = () => {
-      this.setTimeout(() => {
-        setIsLoading(false);
-      }, 3000);
-    }
-
     this._didFocusSubscription = navigation.addListener(  
         'didFocus',
         payload =>
@@ -25,6 +15,7 @@ export default function Main( { navigation } ) {
           )
     );
     const handleDiscardButton = () => {
+
       navigation.navigate('DiscardPage')
     }
 
@@ -33,25 +24,12 @@ export default function Main( { navigation } ) {
       }
     return (
       <Container>
-        {isLoading &&(
-          <AnimatedLoader  visible={true} animationType={'slide'} overlayColor='rgba(21, 219, 10, 1)' 
-          speed={1}  source={require("../../Components/bigLixeira.json")}></AnimatedLoader>
-        // <LottieView  visible={true} style={{backgroundColor: 'rgba(21, 219, 10, 1)' }}
-        // cacheStrategy={'strong'}
-        // source={require("../../Components/city.json")} autoPlay={true} loop={true}></LottieView>
-        )}
-        
-        {toggleLoader()}
   
-        { !isLoading &&(
-        <>  
           <Map>
           </Map>
           <ButtonView>
             <DiscardButton onclick={handleDiscardButton}></DiscardButton>
           </ButtonView>
-        </>
-        )}
       </Container>
     );
 }
